6d1189f4e09207cb0e77b14cc816515c4d850391
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-02 21:51:02 +0530

    list view

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -92,7 +92,23 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Icon(Icons.remove),
                 ),
               ),
-            ])
+            ]),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.map),
+                  title: Text('Map'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo_album),
+                  title: Text('Album'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text('Phone'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
