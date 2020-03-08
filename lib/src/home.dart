ae7021b7b0d83aa9ddd1e79bad9eb1bc629efdae
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-08 23:50:53 +0530

    Update

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -127,7 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Expanded(
                 child: ListTile(
                   leading: Icon(Icons.phone),
-                  title: Text('Phone'),
+                  title: Text('Phone for umber'),
                 ),
               ),
             ]),
