d7ae4d4e748e8fd2a04d9c5c854c20b4c700697a
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-11 23:05:43 +0530

    Auth_event

---
+++ lib/bloc/auth_event.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthEvent extends StatefulWidget {
+  @override
+  _AuthEventState createState() => _AuthEventState();
+}
+
+class _AuthEventState extends State<AuthEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
