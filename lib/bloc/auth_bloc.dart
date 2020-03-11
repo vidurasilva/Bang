15d2dbc30dde0cccd9d1a99a43f26f0c1d9b05d0
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-11 23:05:31 +0530

    auth_bloc

---
+++ lib/bloc/auth_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthBloc extends StatefulWidget {
+  @override
+  _AuthBlocState createState() => _AuthBlocState();
+}
+
+class _AuthBlocState extends State<AuthBloc> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
