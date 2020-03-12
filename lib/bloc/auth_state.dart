c2d059fb9ea98095ed84ce07725247b8d1f563f8
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-12 23:34:54 +0530

    auth state

---
+++ lib/bloc/auth_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthState extends StatefulWidget {
+  @override
+  _AuthStateState createState() => _AuthStateState();
+}
+
+class _AuthStateState extends State<AuthState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
