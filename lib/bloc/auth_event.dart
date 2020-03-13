2ce6157ca28581df9776bf075185485a06b6b956
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-14 01:19:16 +0530

    update auth event

--- lib/bloc/auth_event.dart
+++ lib/bloc/auth_event.dart
@@ -1,13 +1,41 @@
-import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
 
-class AuthEvent extends StatefulWidget {
+abstract class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent();
+
   @override
-  _AuthEventState createState() => _AuthEventState();
+  List<Object> get props => [];
 }
 
-class _AuthEventState extends State<AuthEvent> {
+class AppStarted extends AuthenticationEvent {}
+
+class LoggedIn extends AuthenticationEvent {
+  final String token;
+  final user;
+  const LoggedIn({@required this.token, @required this.user});
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
+  List<Object> get props => [token, user];
+
+  @override
+  String toString() => 'LoggedIn { token: $token userModel: $user }';
+}
+
+class LoggedOut extends AuthenticationEvent {}
+
+class RegisterUser extends AuthenticationEvent {}
+
+class AccessToLoginRegister extends AuthenticationEvent {}
+
+class AccessCodeForLogin extends AuthenticationEvent {
+  final String token;
+
+  const AccessCodeForLogin({@required this.token});
+
+  @override
+  List<Object> get props => [token];
+
+  @override
+  String toString() => 'LoggedWithCode { token: $token}';
 }
