35eabde0c99013fac4975c0fa7afcbfe02037c93
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-15 07:57:13 +0530

    api function calling

--- lib/services/api_function.dart
+++ lib/services/api_function.dart
@@ -1,1 +1,12 @@
-class ApiFunction {}
+import 'package:http/http.dart' as http;
+
+const baseUrl = 'https://rk-eu.10it.lk/richkids/wp-json/';
+
+Future<String> getToken(username, password) async {
+  var url = baseUrl + "jwt-auth/v1/token";
+  http.Response response = await http.post(url, body: {
+    "username": username,
+    "password": password,
+  });
+  return response.body;
+}
