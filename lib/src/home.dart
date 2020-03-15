f773c9a4341b11e971bf28ccf0f43c4691026818
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-16 01:05:44 +0530

    Home dart

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -13,129 +13,29 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   void _decrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.green[300],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Vidura'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            // Image.network(
-            //   'https://github.com/flutter/plugins/raw/master/packages/video_player/doc/demo_ipod.gif?raw=true',
-            // ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: FloatingActionButton(
-                    onPressed: _incrementCounter,
-                    tooltip: 'Increment',
-                    child: Icon(Icons.add),
-                  ),
-                ),
-                Expanded(
-                  child: FloatingActionButton(
-                    onPressed: _decrementCounter,
-                    tooltip: 'Decrement',
-                    child: Icon(Icons.remove),
-                  ),
-                ),
-                // Expanded(
-                //   child: ListView(children: <Widget>[
-                //     ListTile(
-                //       leading: Icon(Icons.map),
-                //       title: Text('Map'),
-                //     ),
-                //     ListTile(
-                //       leading: Icon(Icons.photo_album),
-                //       title: Text('Album'),
-                //     ),
-                //     ListTile(
-                //       leading: Icon(Icons.phone),
-                //       title: Text('Phone'),
-                //     ),
-                //   ]),
-                // ),
-              ],
-            ),
-            Row(children: <Widget>[
-              Expanded(
-                child: ListTile(
-                  leading: Icon(Icons.map),
-                  title: Text('Map'),
-                ),
-              ),
-              Expanded(
-                child: ListTile(
-                  leading: Icon(Icons.photo_album),
-                  title: Text('Album'),
-                ),
-              ),
-              Expanded(
-                child: ListTile(
-                  leading: Icon(Icons.phone),
-                  title: Text('Phone for number'),
-                ),
-              ),
-            ]),
-          ],
+        backgroundColor: Colors.green[400],
+        title: Text(
+          "1000 Bits",
+          style: TextStyle(
+            fontFamily: "Quando",
+          ),
         ),
       ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
