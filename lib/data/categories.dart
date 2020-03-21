55c425e799bd7495026c6fbebc52018af68a0736
vidurasilva <kvidurachathuranga@gmail.com>
2020-03-22 01:28:09 +0530

    catogory model

---
+++ lib/data/categories.dart
@@ -0,0 +1,11 @@
+import 'package:thousand_bits/configs/AppColors.dart';
+import 'package:thousand_bits/models/category.dart';
+
+const List<Category> categories = [
+  Category(name: "Pokedex", color: AppColors.teal),
+  Category(name: "Moves", color: AppColors.red),
+  Category(name: "Abilities", color: AppColors.blue),
+  Category(name: "Items", color: AppColors.yellow),
+  Category(name: "Locations", color: AppColors.purple),
+  Category(name: "Type Charts", color: AppColors.brown),
+];
