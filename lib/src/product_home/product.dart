4299be827422e1a21240b28d36189241b44a940b
vidurasilva <kvidurachathuranga@gmail.com>
2020-04-25 23:51:03 +0530

    Create home page

---
+++ lib/src/product_home/product.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatefulWidget {
+  final String catogory;
+  const ProductPage(String categori, {Key key, this.catogory})
+      : super(key: key);
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<ProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
