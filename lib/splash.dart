538865c6845332d78ff2503dbff1da5cca25b931
vidurasilva <kvidurachathuranga@gmail.com>
2020-05-20 22:47:34 +0530

    Splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:thousand_bits/src/home/home.dart';
 
@@ -23,28 +22,25 @@ class _splashscreenState extends State<splashscreen> {
   // and made the text to align at center
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flare Welcome',
-      home: SplashScreen(
-        'assets/splash.flr',
-        // HomeView(),
-        startAnimation: 'intro',
-        backgroundColor: Color(0xff181818),
+    // return SplashScreen.navigate(
+    //   name: 'intro.flr',
+    //   next: (_) => HomeView(),
+    //   until: () => Future.delayed(Duration(seconds: 5)),
+    //   startAnimation: '1',
+    // );
+    return Scaffold(
+      backgroundColor: Colors.green[250],
+      body: Center(
+        child: Text(
+          "It open your 1000 hopes, Join with Us and bit",
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: "Satisfy",
+          ),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
-    // return Scaffold(
-    //   backgroundColor: Colors.green[250],
-    //   body: Center(
-    //     child: Text(
-    //       "It open your 1000 hopes, Join with Us and bit",
-    //       style: TextStyle(
-    //         fontSize: 50.0,
-    //         color: Colors.white,
-    //         fontFamily: "Satisfy",
-    //       ),
-    //       textAlign: TextAlign.center,
-    //     ),
-    //   ),
-    // );
   }
 }
